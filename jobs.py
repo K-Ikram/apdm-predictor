ddadb59dfbb1328717a4dcc0b0c9f93c9ce987e1
Sarra BOUHENNI <Sarra BOUHENNI>
2017-02-27 14:31:18 +0100

    predictor 27/02

--- jobs.py
+++ jobs.py
@@ -5,9 +5,16 @@ Created on Mon Feb 20 11:43:32 2017
 @author: BOUEHNNI
 """
 
-from crontab import CronTab
-file_cron = CronTab(tabfile='filename.tab')
-job  = file_cron.new(command='python Services/DiseaseForcasting.py')
-job.hour.every(4)
-job.enable(False)
-list = file_cron.find_command('python Services/DiseaseForcasting.py')
+import schedule
+import time
+
+def job():
+    print("I'm working...")
+
+schedule.every(1).minutes.do(job)
+schedule.every().hour.do(job)
+schedule.every().day.at("10:30").do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
