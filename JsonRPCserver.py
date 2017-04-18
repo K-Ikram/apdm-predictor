32d9ba9c0306ff027dbc21932bb799a64166bcc0
Sarra BOUHENNI <Sarra BOUHENNI>
2017-04-18 14:55:25 +0100

    predicteur avec jsonrpcserver

--- JsonRPCserver.py
+++ JsonRPCserver.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-
+import sys
+from jsonrpclib.SimpleJSONRPCServer import PooledJSONRPCServer
+from jsonrpclib.threadpool import ThreadPool
 from Learner.WeightedKNNLearner import WeightedKNNLearner
 from DataAccess.MongoDB.AbstractLearningDataAccess import AbstractLearningDataAccess
 
@@ -22,18 +24,7 @@ def getRiskRates(crop_production, disease):
     risk_rates = AbstractLearningDataAccess.getInstance().getRiskRates(crop_production, disease)
     print risk_rates
     return risk_rates
-    
-    
-#server = SimpleJSONRPCServer(('localhost', 8778))
-#print "Json RPC server has been launched"
-#server.register_function(penalize)
-#server.register_function(reward)
-#server.register_function(getLastRiskRate)
-#server.register_function(getRiskRates)
 
-
-from jsonrpclib.SimpleJSONRPCServer import PooledJSONRPCServer
-from jsonrpclib.threadpool import ThreadPool
 
 # Setup the notification and request pools
 notif_pool = ThreadPool(max_threads=10, min_threads=0)
@@ -42,9 +33,9 @@ request_pool = ThreadPool(max_threads=50, min_threads=10)
 # Don't forget to start them
 notif_pool.start()
 request_pool.start()
-
+port_num = int(sys.argv[1])
 # Setup the server
-server = PooledJSONRPCServer(('localhost', 8750), thread_pool=request_pool)
+server = PooledJSONRPCServer(('localhost', port_num), thread_pool=request_pool)
 server.set_notification_pool(notif_pool)
 
 # Register methods
@@ -54,7 +45,7 @@ server.register_function(getLastRiskRate)
 server.register_function(getRiskRates)
 
 try:
-    print "Json RPC server has been launched"
+    print "Json RPC server has been launched on port: ",port_num
     server.serve_forever()
     
 finally:
